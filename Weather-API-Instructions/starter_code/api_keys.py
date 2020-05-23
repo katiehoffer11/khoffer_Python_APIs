9a5d379d3aec8a4a728a2a018a81a5d35b6fd768
katie hoffer <katiehoffer3@gmail.com>
2020-05-23 15:06:04 -0400

    adding homework

---
+++ Weather-API-Instructions/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "65bc94fec8a04c9d701f43f2a0383b13"
+
+# Google API Key
+g_key = "AIzaSyBu9zd_L3i5qGbVRwtcnz639eOIPpzVvO4"
---
+++ config.py
@@ -0,0 +1,13 @@
+#Twitter API Keys 
+consumer_key = '5WpmwkBccYL3cPnonk50lJRsJ'
+consumer_secret = "Zc6nz1f6vwWykTFPj1FxYPVQEuLYglZi4JiBwL9bWLByqeb0GW"
+access_token = "1194798405450645506-ofCNOvPfN2yNqOTfnA3MFCgNoNoPKV"
+access_token_secret = "MnV7CYuIXFXJIe9OyJF6eI75zatDafeEZAMdSVlAXqXDI"
+
+#Weather 
+weather_api_key = '65bc94fec8a04c9d701f43f2a0383b13'
+
+#Census
+
+
+
